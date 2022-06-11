44219acf58c7cd286407bf08f4216608de6c0762
BigTzu <joshuel.kuilwijk@epitech.eu>
2022-06-11 19:15:11 +0900

    ADD API Flask and gitignore

--- py/main.py
+++ py/main.py
@@ -1,21 +1,31 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from flask import Flask, request, jsonify
 
 cred = credentials.Certificate("hallowed-welder-297014-042131c68dd6.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-def add_user_to_firestore(username, password, email):
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+     return "AnimEngine root"
+
+
+@app.route('/users', methods=['POST'])
+def add_user_to_firestore():
     doc_ref = db.collection(u'AnimEngine').document()
     doc_ref.set({
-        u'email': email,
-        u'username': username,
-        u'password': password
+        u'email': request.json["email"],
+        u'username': request.json["username"],
+        u'password': request.json["password"]
     })
+    return jsonify(doc_ref.get().to_dict())
+
 
 if __name__ == '__main__':
-    email = input("Enter email: ")
-    username = input("Enter username: ")
-    password = input("Enter password: ")
-    add_user_to_firestore(username, password, email)
+    app.run(debug=True, port=8080)
